a5da94b3c49e02b61fe7fb2be14740c88b1c82f6
takurinton <taryochanmannn@icloud.com>
2021-03-09 13:19:27 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <br>
+    <h1>harunonsan no yatu</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+}
+.logo {
+  width: 200px;
+  height: 200px;
+  display: inline-block;
+}
+</style>
