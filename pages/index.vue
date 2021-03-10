f42fb22ba847f0353798e39edd665d28dc770a51
takurinton <taryochanmannn@icloud.com>
2021-03-11 00:05:26 +0900

    bind double

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
 <div>
-  <input type="text" id="name" v-model="name" @input="convert_kana"><br />
-  <input type="text" id="furigana" v-model="furigana">
+  <div>
+    <h2>1</h2>
+    <input type="text" id="name" v-model="name" @input="convert_kana">
+    <input type="text" id="furigana" v-model="furigana">
+  </div>
+  <div>
+    <h2>2</h2>
+    <input type="text" id="_name" v-model="_name" @input="convert_kana">
+    <input type="text" id="_furigana" v-model="_furigana">
+  </div>
 </div>
 </template>
 
@@ -17,16 +25,20 @@ export default {
     return {
       form: {
         name: "",
-        furigana: ""
+        furigana: "", 
+        _name: "", 
+        _furigana: ""
       }
     }
   },
   mounted() {
     autokana = AutoKana.bind("#name", "#furigana", { katakana: true })
+    autokana = AutoKana.bind("#_name", "#_furigana", { katakana: true })
   },
   methods: {
     convert_kana() {
       this.furigana = autokana.getFurigana()
+      this._furigana = autokana.getFurigana()
     }
   }
 }
